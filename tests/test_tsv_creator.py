c14e139d191d33908586f7145b02193dd4e5cd22
Tar-Calion <43185234+Tar-Calion@users.noreply.github.com>
2024-02-12 22:43:01 +0100

    move book title to the front of the TSV

--- tests/test_tsv_creator.py
+++ tests/test_tsv_creator.py
@@ -18,9 +18,9 @@ class TestCsvCreator(unittest.TestCase):
     def test_create(self):
         result = self.creator.create(self.books)
         expected_output = (
-            '"Test Author 1"\t"Test Title 1"\t"2021"\t"Test Description 1"\t"Test Label 1"\t"Humble Bundle"\t"PDF, EPUB"\n'
-            '"Test Author 2"\t"Test Title 2"\t"2020"\t"Test Description 2"\t"Test Label 2, Test Label 3"\t"Humble Bundle"\t"PDF"\n'
-            '"Test Author 3"\t"Test Title 3"\t""\t"Test Description 3 "\t""\t"Humble Bundle"\t"PDF, EPUB, MOBI"\n'
+            '"Test Title 1"\t"Test Author 1"\t"2021"\t"Test Description 1"\t"Test Label 1"\t"Humble Bundle"\t"PDF, EPUB"\n'
+            '"Test Title 2"\t"Test Author 2"\t"2020"\t"Test Description 2"\t"Test Label 2, Test Label 3"\t"Humble Bundle"\t"PDF"\n'
+            '"Test Title 3"\t"Test Author 3"\t""\t"Test Description 3 "\t""\t"Humble Bundle"\t"PDF, EPUB, MOBI"\n'
         )
         self.assertEqual(result, expected_output)
 
