9b18b0bd1d9b078ead6d9535d0397d8dc3e6375c
Tar-Calion <43185234+Tar-Calion@users.noreply.github.com>
2024-02-12 22:33:06 +0100

    improve description parsing

--- extractor/books_extractor.py
+++ extractor/books_extractor.py
@@ -5,9 +5,11 @@ from extractor.book import Book
 class BooksExtractor:
 
     # Function to replace line breaks with a space, adding a space if the line does not end with one
-    def _replace_linebreaks_with_space(self, text):
+    def _butify_description(self, text):
         lines = text.split('\n')
-        return ' '.join(line if line.endswith(' ') else line + ' ' for line in lines)
+        joined_description = ' '.join(line if line.endswith(' ') else line + ' ' for line in lines)
+
+        return ' '.join(joined_description.split())
 
     def _get_formats(self, title_tag):
         # Find all 'delivery-and-oses' div tags relative to the current title tag
@@ -43,7 +45,7 @@ class BooksExtractor:
             # Find the nearest description tag relative to the current title tag
             description_tag = title_tag.find_next('section', class_='description')
             description = description_tag.get_text(strip=True) if description_tag else 'No description available'
-            description = self._replace_linebreaks_with_space(description)
+            description = self._butify_description(description)
 
             formats = self._get_formats(title_tag)
 
